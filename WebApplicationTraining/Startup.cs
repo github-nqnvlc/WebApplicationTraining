540308b7fb2ee9089716a0333be056eba12b9f28
= <=>
2021-06-20 17:02:29 +0700

    Update Startup

--- WebApplicationTraining/Startup.cs
+++ WebApplicationTraining/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using WebApplicationTraining.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplicationTraining.Startup))]
 namespace WebApplicationTraining
@@ -9,6 +12,55 @@ namespace WebApplicationTraining
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+
+        }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            // In Startup iam creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            var user = new ApplicationUser();
+            user.UserName = "admin";
+            user.Email = "admin@gmail.com";
+            string userPassword = "Password@123";
+            var checkUser = UserManager.Create(user, userPassword);
+            if (checkUser.Succeeded)
+            {
+                var result1 = UserManager.AddToRole(user.Id, "Admin");
+            }
+            // creating Creating TrainingStaff role     
+            if (!roleManager.RoleExists("TrainingStaff"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "TrainingStaff";
+                roleManager.Create(role);
+
+            }
+            // creating Creating Trainer role     
+            if (!roleManager.RoleExists("Trainer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Trainer";
+                roleManager.Create(role);
+            }
+            // creating Creating Trainee role     
+            if (!roleManager.RoleExists("Trainee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Trainee";
+                roleManager.Create(role);
+            }
         }
     }
 }
